95a74fad337d815e5734348b61f5ae7355a0858a
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-11-26 10:18:15 +0100

    O(n^2): log all pairs in an array

---
+++ big-o/O(n^2)/log-all-pairs-in-an-array/main.js
@@ -0,0 +1,8 @@
+
+const sampleArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+for (let i = 0; i < sampleArray.length; i++) {
+    for (let j = 0; j < sampleArray.length; j++) {
+        console.log('Sample Array', i, 'tem:', j)
+    }
+}
