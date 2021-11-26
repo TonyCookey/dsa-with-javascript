7f8b7d9321808e51e58610049d089744fe95d394
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-11-26 10:18:42 +0100

    Fix: O(n^2): updated code for (log all pairs in an array)

--- big-o/O(n^2)/log-all-pairs-in-an-array/main.js
+++ big-o/O(n^2)/log-all-pairs-in-an-array/main.js
@@ -3,6 +3,6 @@ const sampleArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 for (let i = 0; i < sampleArray.length; i++) {
     for (let j = 0; j < sampleArray.length; j++) {
-        console.log('Sample Array', i, 'tem:', j)
+        console.log('Sample Array', sampleArray[i], 'Item:', sampleArray[j])
     }
 }
