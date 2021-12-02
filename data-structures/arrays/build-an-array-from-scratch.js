4d20fc7e4340efa86be6ca9809936dec08aa7441
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-02 12:23:00 +0100

    Updated build array from scratch

--- data-structures/arrays/build-an-array-from-scratch.js
+++ data-structures/arrays/build-an-array-from-scratch.js
@@ -25,7 +25,7 @@ class arrayFromScratch {
         for (let i = index; i < this.length - 1; i++) {
             this.data[i] = this.data[i + 1]
         }
-        // delete this.data[this.length - 1]
+        delete this.data[this.length - 1]
         this.length--
     }
 
