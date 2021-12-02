20adba3eb6e48982df4bded60c9f7e83ed2eccc5
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-02 16:13:42 +0100

    update: returned the string rather than an array of words and updated the method to reverse every word in the string

--- problem-solving/reverse-a-string/index.js
+++ problem-solving/reverse-a-string/index.js
@@ -1,15 +1,15 @@
 // Reverse a string 
 
-function reverseString(randomString) {
+function reverseString(randomString, sep) {
     // Convert string to array using string.split()
     // Time Complexity: O(n)
-    randomStringArray = randomString.split(' ')
+    randomStringArray = randomString.split(sep)
     let resultArray = []
     for (let i = randomStringArray.length - 1; i >= 0; i--) {
         resultArray.push(randomStringArray[i])
     }
-    return resultArray
+    return resultArray.join(sep)
 }
 const randomString = 'My name is Tony Cookey'
-const result = reverseString(randomString)
+const result = reverseString(randomString, '')
 result
