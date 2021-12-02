f373f49273d8f33cfb60d9d55f72cd7c6631bb3e
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-02 12:22:18 +0100

    updated and completed array from scratch

--- data-structures/arrays/build-an-array-from-scratch.js
+++ data-structures/arrays/build-an-array-from-scratch.js
@@ -18,20 +18,30 @@ class arrayFromScratch {
     }
     delete(index) {
         const item = this.data[index]
-        this.shiftItems()
+        this.shiftItems(index)
         return item
     }
-    shiftItems() { }
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1]
+        }
+        // delete this.data[this.length - 1]
+        this.length--
+    }
 
 
 }
 
 let arr1 = new arrayFromScratch()
 
+arr1.push('item0')
 arr1.push('item1')
 arr1.push('item2')
+arr1.push('item3')
+arr1.push('item4')
 let result = arr1.get(0)
 result
 arr1
-arr1.pop()
+// // arr1.pop()
+arr1.delete(1)
 arr1
