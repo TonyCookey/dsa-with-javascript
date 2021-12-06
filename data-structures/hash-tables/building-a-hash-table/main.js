272aca05b7ecf86ff8a530fc5745d4c91780d5e4
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-06 09:47:40 +0100

    build a hash table from scratch

---
+++ data-structures/hash-tables/building-a-hash-table/main.js
@@ -0,0 +1,31 @@
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size);
+    }
+
+    _hash(key) {
+        let hash = 0;
+        for (let i = 0; i < key.length; i++) {
+            hash = (hash + key.charCodeAt(i) * i) % this.data.length
+        }
+        return hash;
+    }
+    set(key, value) {
+        let bucket = []
+        // push the key and value into the bucket
+        let hashedKey = this._hash(key)
+        bucket.push(key, value)
+
+        // save the bucket in the data Array
+        this.data[hashedKey] = bucket
+    }
+    get(key) {
+        let hashedKey = this._hash(key)
+        return this.data[hashedKey][1]
+    }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000)
+const result = myHashTable.get('grapes')
+result
