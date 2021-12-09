1caa595befbe731e4e86d75db5db9a81be288d01
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-09 11:54:27 +0100

    building a linked list - insert method

--- data-structures/linked-lists/main.js
+++ data-structures/linked-lists/main.js
@@ -27,8 +27,51 @@ class LinkedList {
         }
         this.length++
     }
+    printList() {
+        const array = []
+        let currentNode = this.head
+        while (currentNode !== null) {
+            array.push(currentNode.value)
+            currentNode = currentNode.next
+        }
+        return array
+    }
+    insertAt(index, value) {
+        if (index == 0) {
+            return this.prepend(value)
+        }
+        if (index >= this.length) {
+            return this.append(value)
+        }
+        // get the node leading/preceeding the node at the index you want to insert at
+        const leader = this.traverseTill(index - 1)
+        const nodeAtIndex = leader.next
+        //insert the new node at next of the leader
+        leader.next = {
+            value,
+            next: null
+        }
+        this.length++
+        // add the follwing nodes athe back of the new node
+        leader.next.next = nodeAtIndex
+
+
+    }
+    traverseTill(index) {
+        let counter = 0
+        let currentNode = this.head
+        while (counter !== index) {
+            currentNode = currentNode.next
+            counter++
+        }
+        return currentNode
+    }
+
 }
 
+// { head: { value: 1, next: { value: 10, next: [Object] } },
+//   tail: { value: 19, next: null },
+//   length: 6 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
@@ -36,5 +79,8 @@ myLinkedList.append(16);
 myLinkedList.append(12);
 myLinkedList.append(19);
 myLinkedList.prepend(1);
+myLinkedList
+console.log(myLinkedList.insertAt(9, 14))
+console.log(myLinkedList.printList())
 console.log(myLinkedList);
 
