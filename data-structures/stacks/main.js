8c2ddcc9c8d4b2b1fcf76384877d84ce88fa9897
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-16 11:57:08 +0100

    updated the stack implementation

--- data-structures/stacks/main.js
+++ data-structures/stacks/main.js
@@ -12,11 +12,15 @@ class Stack {
         this.length = 0;
     }
     peek() {
+        console.log(this.top.value);
         return this.top
     }
     push(value) {
         let newNode = new Node()
         newNode.value = value
+        if (this.length == 0) {
+            this.bottom = newNode
+        }
         newNode.next = this.top
         this.top = newNode
         this.length++
@@ -27,9 +31,16 @@ class Stack {
         this.length--
     }
     isEmpty() {
+        console.log(this.length == 0);
         return this.length == 0
     }
 
 }
 
 const myStack = new Stack();
+myStack.push("Tony")
+myStack.push("Cookey")
+myStack.push("Starboy")
+myStack.peek()
+myStack.isEmpty()
+myStack
