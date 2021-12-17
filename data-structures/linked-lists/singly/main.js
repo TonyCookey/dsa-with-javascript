e64a04b8789b90ef7b8d84c691523b828fcc49ef
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-17 23:32:46 +0100

    update the singly linked list code

--- data-structures/linked-lists/singly/main.js
+++ data-structures/linked-lists/singly/main.js
@@ -4,7 +4,7 @@ class LinkedList {
     constructor(value) {
         this.head = {
             value: value,
-            next: null,
+            next: null
         };
         this.tail = this.head;
         this.length = 1;
@@ -25,7 +25,6 @@ class LinkedList {
             value,
             next: this.head
         }
-        this.head.next = this.head
         this.length++
     }
     printList() {
@@ -47,30 +46,14 @@ class LinkedList {
         // get the node leading/preceeding the node at the index you want to insert at
         const leader = this.traverseTill(index - 1)
         const nodeAtIndex = leader.next
-
-        const newNode = {
+        //insert the new node at next of the leader
+        leader.next = {
             value,
-            next: nodeAtIndex
+            next: null
         }
-        leader.next = newNode
         this.length++
-    }
-    remove(index) {
-
-        if (index < 0 || index > this.length) {
-            return 'invalid index'
-        }
-
-        // get the node leading/preceeding the node at the index you want to remove at
-        const leader = this.traverseTill(index - 1)
-
-        // get the toBeRemovedNode
-        const toBeRemovedNode = leader.next
-
-        // set the node follwing the toBeRemovedNode as the next node to the leader node 
-        leader.next = toBeRemovedNode.next
-
-        this.length--
+        // add the follwing nodes athe back of the new node
+        leader.next.next = nodeAtIndex
     }
     traverseTill(index) {
         let counter = 0
@@ -84,16 +67,13 @@ class LinkedList {
 
 }
 
-
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
 myLinkedList.append(12);
 myLinkedList.append(19);
 myLinkedList.prepend(1);
-myLinkedList.insertAt(2, 14)
-console.log(myLinkedList.printList())
-myLinkedList.remove(2)
+myLinkedList
+myLinkedList.insertAt(9, 14)
 console.log(myLinkedList.printList())
 console.log(myLinkedList);
-
