9040d5c0d00edb812281c03edb095afca710315d
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-18 19:16:53 +0100

    WIP: working on the remove function in BST

--- data-structures/binary-search-tree/main.js
+++ data-structures/binary-search-tree/main.js
@@ -56,7 +56,66 @@ class BinarySearchTree {
         }
         return false
     }
-    // remove
+    isLeaf(node) {
+        if (node.left == true && node.right == true && node.value !== null) {
+            return true
+        }
+        return false
+    }
+    hasOneChild(node) {
+        if (node.left == true || node.right == true) {
+            return true
+        }
+        return false
+    }
+    remove(value) {
+        let nodePointer = this.root
+        let predecessor = null
+        let successor = null
+
+        while (nodePointer != null) {
+            if (nodePointer.value == value) {
+                // if the node is a leaf node - at the very end
+                if (this.isLeaf(nodePointer)) {
+                    if (value > predecessor.value) {
+                        predecessor.right = null
+                    } else {
+                        predecessor.left = null
+                    }
+                    return
+                }
+                if (this.hasOneChild(nodePointer)) {
+                    if (value < predecessor.value) {
+                        predecessor.left = nodePointer.left
+                    } else {
+                        predecessor.right = nodePointer.right
+                    }
+                    return
+                }
+                successor = nodePointer.right
+                while (this.isLeaf(successor)) {
+                    successor = successor.left
+                }
+
+                successor.left = nodePointer.left
+                successor.right = nodePointer.right
+                if (predecessor.value > successor.value) {
+                    predecessor.left = successor
+                } else {
+                    predecessor.right = successor
+                }
+                return
+            }
+            if (value < nodePointer.value) {
+                nodePointer = nodePointer.left
+            }
+            else {
+                nodePointer = nodePointer.right
+            }
+            predecessor = nodePointer
+
+        }
+    }
 }
 
 function traverse(node) {
