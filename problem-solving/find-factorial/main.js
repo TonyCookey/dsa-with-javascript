4e6dce1aa08d8ecb9aa5bd90522629ab92d958f7
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-21 13:52:31 +0100

    solved factorial using both recursion and factorial

---
+++ problem-solving/find-factorial/main.js
@@ -0,0 +1,19 @@
+
+function findFactorialUsingRecursion(x) {
+    if (x < 1) {
+        return 1
+    }
+    return x * findFactorialUsingRecursion(x - 1)
+
+}
+
+function findFactorialUsingIteration(x) {
+    let sum = 1
+    while (x > 1) {
+        sum = sum * x
+        x--
+    }
+    return sum
+}
+const result = findFactorialUsingIteration(6)
+result
