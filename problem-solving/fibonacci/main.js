cd5f8074fe970d168021df3dce933403b04b2459
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-22 10:50:33 +0100

    solved fibonacci using two iteration approaches

--- problem-solving/fibonacci/main.js
+++ problem-solving/fibonacci/main.js
@@ -8,6 +8,26 @@ function fibonacciRecursive(n) {
     return sum
 }
 
-function fibonacciIterative(params) {
+function fibonacciIterative(n) {
+    let next
+    let prev = 0
+    let current = 1
+    let index = 1
 
+    while (n > index) {
+        next = current + prev
+        prev = current
+        current = next
+        index++
+    }
+    return current
 }
+function fibonacciIterativeArray(n) {
+    let arr = [0, 1]
+    for (let index = 2; index <= n; index++) {
+        arr.push(arr[index - 2] + arr[index - 1])
+    }
+    return arr[n]
+}
+const result = fibonacciIterativeArray(9)
+result
