456f9056f35aeb799ac1f8fb3696fd7b739572c5
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-23 20:22:16 +0100

    insertion sort algorithm in quadratic time

---
+++ algorithms/sorting/insertion-sort/main.js
@@ -0,0 +1,22 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > array[i + 1]) {
+            let holding = array[i]
+            array[i] = array[i + 1]
+            array[i + 1] = holding
+            for (let j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    holding = array[j]
+                    array[j] = array[j - 1]
+                    array[j - 1] = holding
+                }
+            }
+        }
+    }
+    return array
+}
+
+const result = insertionSort(numbers);
+result
