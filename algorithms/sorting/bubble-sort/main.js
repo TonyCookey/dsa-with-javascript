ce685b5b9df85a97b474ba54454fc08c53255d01
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-23 16:48:59 +0100

    updated bubble sort algorithm

--- algorithms/sorting/bubble-sort/main.js
+++ algorithms/sorting/bubble-sort/main.js
@@ -1,11 +1,11 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function bubbleSort(array) {
+function bubbleSort(numbers) {
     // bubble sort algorthm in 0(n^2)- quadratic time
     for (let i = 0; i < numbers.length; i++) {
         let start = numbers[0];
         let next
-        for (let j = 1; j < array.length; j++) {
+        for (let j = 1; j < numbers.length; j++) {
             next = numbers[j]
             if (start > next) {
                 numbers[j - 1] = next
