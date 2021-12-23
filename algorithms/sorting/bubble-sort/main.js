ebd8cecb252736d16060c3050f5f68cc8f28930b
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-23 14:46:33 +0100

    bubble sort algorthm in 0(n^2)- quadratic time

---
+++ algorithms/sorting/bubble-sort/main.js
@@ -0,0 +1,23 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function bubbleSort(array) {
+    // bubble sort algorthm in 0(n^2)- quadratic time
+    for (let i = 0; i < numbers.length; i++) {
+        let start = numbers[0];
+        let next
+        for (let j = 1; j < array.length; j++) {
+            next = numbers[j]
+            if (start > next) {
+                numbers[j - 1] = next
+                numbers[j] = start
+            }
+            else {
+                start = numbers[j]
+            }
+
+        }
+    }
+}
+
+bubbleSort(numbers);
+console.log(numbers);
