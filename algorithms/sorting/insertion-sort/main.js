4d2c2215b5f45e2a1e809c43da30a983140f9114
Anthony Iroegbu <anthony@crenettechlabs.com>
2021-12-23 20:39:01 +0100

    updated insertion sort algorithm input

--- algorithms/sorting/insertion-sort/main.js
+++ algorithms/sorting/insertion-sort/main.js
@@ -1,4 +1,4 @@
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+const numbers = [99, 44, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function insertionSort(array) {
     for (let i = 0; i < array.length - 1; i++) {
